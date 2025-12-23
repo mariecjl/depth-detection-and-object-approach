45bbf5e557d50a93cecfcbdaef52da6bcd96b96d
marie <168664981+mariecjl@users.noreply.github.com>
2025-12-23 15:52:27 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+from collections import deque
+
+DEPTH_HISTORY = 8
+APPROACH_FRAMES_REQUIRED = 4
+
+MIN_AREA_RATIO = 0.03
+CENTER_WEIGHT_RADIUS = 0.40
+
+DELTA_HIGH = 0.35
+DELTA_LOW = 0.15
+
+depth_buffer = deque(maxlen=DEPTH_HISTORY)
